c299e93ee2788ed9a98599198a8c3b3ce7eb1266
Dev kumar <dkumar66043@gmail.com>
2025-10-24 12:11:12 +0530

    Add initial implementation for numpy array dimensions

---
+++ introduction/Numpy Array Properties1/ndim.py
@@ -0,0 +1,14 @@
+##number diemention
+
+import numpy as np
+
+arr_1d= np.array([1,2,3])
+arr_2d= np.array([[1,2,3],[4,5,6]])
+arr_3d= np.array([[[1,2],[3,4],[5,6],[7,8]]])
+
+
+print(arr_1d.nidm)
+print(arr_2d.nidm)
+print(arr_3d.nidm)
+
+                  
