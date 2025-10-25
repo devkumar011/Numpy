c7ad2b64af06d4ac362dc86ad71b9988d7b04446
Dev kumar <dkumar66043@gmail.com>
2025-10-25 09:50:27 +0530

    Add initial implementations for numpy array operations and aggregation functions

---
+++ introduction/agg_fun.py
@@ -0,0 +1,18 @@
+##Aggregation means summarizing or combining data ? in other words, taking a large dataset 
+# and producing a single meaningful value from it (like sum, mean, min, max, etc.).
+
+
+
+
+import numpy as np
+
+arr = np.array([10,20,30,40,50])
+
+
+print(np.sum(arr))
+print(np.mean(arr))
+print(np.min(arr))
+print(np.max(arr))
+print(np.std(arr))
+print(np.var(arr))
+
